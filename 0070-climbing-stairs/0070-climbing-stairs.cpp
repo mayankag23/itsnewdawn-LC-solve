b4f1ef329bf6bb577255649df2869a6b33d1ef6c
Mayank Agrawal <153766256+mayankag23@users.noreply.github.com>
2026-08-15 13:14:29 +0530

    Time: 0 ms (100%), Space: 7.7 MB (99.03%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==1 || n==2) return n;
+        int prev1 = 1;
+        int prev2 = 2;
+        int curr;
+        for(int i =2; i<n; i++){
+            curr= prev1 + prev2;
+            prev1 = prev2;
+            prev2 = curr;         
+        }        
+        return curr;
+    }
+};
